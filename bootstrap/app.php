91f97ad54d99fd9eb0a6c52b5c9d0cb730631df7
abil0321 <74514963+abil0321@users.noreply.github.com>
2025-08-05 09:29:09 +0700

    studied: Laravel Router

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,12 +6,16 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // * Setting agar method post bisa berjalan di Route
+        $middleware->validateCsrfTokens(except: [
+            // '/movie' // validateCsrfTokens tidak akan aktif kecuali untuk route /movie
+            '*' // menggunakan '*' agar tidak aktif ke semua route
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,56 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+$movies = [];
+for ($i = 0; $i < 3; $i++) {
+    $movies[] = [
+        'name' => 'Title ' . $i,
+        'year' => 2001,
+        'genre' => 'Genre'
+    ];
+};
+
+Route::get('/movie', function () use ($movies) {
+    return $movies;
+});
+
+// * Route POST: membuat data baru
+//* note: ada settingan di bootstrap/app.php untuk menjalankan method post nya. Semacam Validasi Baru
+Route::post('/movie', function () use ($movies) {
+    // * mengambil semua request dari form
+    // return request()->all();
+
+    // *mengambil request sesuai dengan attribute/key
+    $movies[] = [
+        'name' => request('name'),
+        'year' => request('year'),
+        'genre' => request('genre')
+    ];
+    return $movies;
+});
+
+// *  Route PUT: Edit data by index menggunakan Route PUT
+Route::put('/movie/{id}', function ($id) use ($movies) {
+    $movies[$id]['name'] = request('name');
+    $movies[$id]['year'] = request('year');
+    $movies[$id]['genre'] = request('genre');
+
+    return $movies;
+});
+
+// *  Route PATCH: Edit data by index menggunakan Route PATCH
+Route::patch('/movie/{id}', function ($id) use ($movies) {
+    $movies[$id]['name'] = request('name');
+    $movies[$id]['year'] = request('year');
+    $movies[$id]['genre'] = request('genre');
+
+    return $movies;
+});
+
+
+// * Route Delete: Menghapus data by index
+Route::delete('/movie/{id}', function ($id) use ($movies) {
+    unset($movies[$id]);
+    return $movies;
+});
