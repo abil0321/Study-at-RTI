5c0a245bffd7571348b3519f450fea886af67117
abil0321 <74514963+abil0321@users.noreply.github.com>
2025-08-06 10:14:57 +0700

    finish materi request Laravel 11, requet untuk mengakses data request http web/di web

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\WorkingController;
 use App\Http\Middleware\CheckPegawai;
 use Cassandra\Index;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -33,3 +34,64 @@ Route::get('/career', function () {
 Route::get('login', function () {
     return 'Halaman Login';
 })->name('login');
+
+// * Bermain dengan request =====================================================
+Route::get('/request', function (Request $request) {
+    $user = $request->all();
+    // dd($user);
+    return $user['nik'];
+});
+
+Route::post('/request', function (Request $request) {
+    return $request->all();
+});
+
+Route::post('/request/input', function (Request $request) {
+    // * dapat all data
+    // $input = $request->input();
+
+    // * dapat all data color
+    $input = $request->input('color.*');
+    return $input;
+});
+
+Route::post('/request/query', function (Request $request) {
+    // * cuma dapat data query nya saja / data dari url
+    $query = $request->query('gender');
+    return $query;
+});
+
+Route::post('/request/date', function (Request $request) {
+    // * untuk format date
+    $date = $request->date('schedule', 'Y-m-d', 'Asia/Jakarta')->addDay()->addMinutes(30)->addHours(3)->format('Y-m-d H:i:s');
+    return $date;
+    // return $date->diffForHumans();
+});
+
+// * mengecek keberadaan property
+Route::post('/request/has', function (Request $request) {
+    if ($request->has(['name', 'email'])) {
+        return 'Data ada, Login Berhasi';
+    }
+
+    if ($request->hasAny(['name', 'email'])) {
+        return 'Datanya ada bang!';
+    }
+
+    return 'gagal';
+});
+
+// * mengecek ke tidak adaan property
+Route::post('/request/missing', function (Request $request) {
+    if ($request->missing(['name', 'email'])) {
+        return 'Datanya tidak ada bang';
+    }
+
+    return 'Datanya ada bang!';
+});
+
+Route::post('/request/merge', function (Request $request) {
+    // * bakalan ngereplace yang ada, mau datanya awalnya null atau tidak, ada atau tidak
+    $merge = $request->merge(['email' => 'budi@gmail.com']);
+    return $request->input();
+});
