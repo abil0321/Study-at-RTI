e27a49a3c045cc4607c894f862b706aa4690de05
abil0321 <74514963+abil0321@users.noreply.github.com>
2025-10-13 06:25:37 +0700

    first commit: study codeigniter3

--- resources/views/components/partials/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="mt-5 bg-gray-800 p-5">
-    <div class="flex justify-center">
-        <a href="{{ route('working.index') }}" class="text-center text-2xl font-bold">Working Seekers</a>
-    </div>
-    <p class="px-3 py-2 text-center">Footer & Created by: Muhammad Salsabil</p>
-</footer>
--- resources/views/components/partials/header.blade.php
+++
@@ -1,4 +0,0 @@
-<header class="flex justify-between bg-gray-800 p-5">
-    <a href="{{ route('working.index') }}" class="text-2xl font-bold">Working Seekers</a>
-    <a href="{{ route('working.create') }}" class="rounded bg-blue-600 px-3 py-2 hover:bg-blue-500">Add</a>
-</header>
