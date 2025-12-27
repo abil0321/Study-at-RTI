9d26675e4b98e514d9cc510154555fa722f9ccfc
Abil <salsabilmuhammad45@gmail.com>
2025-12-27 17:18:32 +0800

    feat: refactor HomePage to use spread attributes for Article component

--- react-fundamental/src/pages/index.jsx
+++ react-fundamental/src/pages/index.jsx
@@ -4,8 +4,14 @@ function HomePage() {
   return (
     <>
       <h1>Simple Blog</h1>
-      {posts.map((post) => {
-          return <Article title={post.title} tags={post.tags} date={post.date}/>
+      {posts.map(({title, date, tags}, index) => { // NOTE: Memasukkan item nya kedalam parameter biar tidak usah seperti ini "post.title post.title ... dst"
+          // return <Article title={title} tags={tags} date={date}/>
+          
+          // TODO: menggunakan spread attributes
+          // NOTE: kasusnya mirip seperti ini, jadi tuh nanti hasilnya "title={title} date={date} ...dst"
+          // const title = "a";
+          // const a = {title}
+          return <Article {...{title, date, tags}} key={index}/>
       })}
     </>
   );
