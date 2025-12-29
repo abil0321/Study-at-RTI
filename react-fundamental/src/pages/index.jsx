f54fa8c4d89a6d7ce4ff0f5283e97d01f65382b6
Abil <salsabilmuhammad45@gmail.com>
2025-12-29 21:45:45 +0800

    feat: fetch external data using useEffect and display in HomePage

--- react-fundamental/src/pages/index.jsx
+++ react-fundamental/src/pages/index.jsx
@@ -5,28 +5,42 @@ import { useEffect, useState } from "react";
 function HomePage() {
   const [posts, setPosts] = useState(postsData);
   const [totalPost, setTotalPost] = useState(0);
+  // NOTE: Menggunakan state untuk set external data API
+  const [externalPost, setExternalPost] = useState([]);
   const onSearchChange = (value) => {
     const filteredPost = postsData.filter((item) => item.title.includes(value));
     setPosts(filteredPost);
     setTotalPost(filteredPost.length);
   };
 
+  // TODO: Fetch external data dari API menggunakan useEffect
+  // NOTE: Menggunakan useEffect agar data external di fetch hanya sekali diawal
   useEffect(() => {
-    console.log('render');
-    return () => {
-      console.log('cleanup');
-    }
-  }, [posts]);
+    console.log('fetch externalPost!');
+    
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(res => res.json())
+    .then(json => setExternalPost(json))
+  }, []);
+
+  // NOTE: Double useEffect jika ada yang ingin dijalankan ketika suatu kondisi terpenuhi
+  useEffect(() => {
+    console.log('ada pencarian post!');
+  }, [posts]) // NOTE: ini triggernya, Pencarian post akan dijalankan ketika posts diubah
 
   return (
     <>
       <h1>Simple Blog</h1>
       <Search searchChange={onSearchChange} totalData={totalPost} />
       {posts.map((props, index) => {
-        // TODO: menggunakan spread attributes
-        // NOTE: kasusnya mirip seperti ini, jadi tuh nanti hasilnya "title={post.title} date={post.date} ...dst"
         return <Article {...props} key={index} />;
       })}
+
+      <hr/>
+
+      {externalPost.map((item, index) => (
+        <div key={index}>- {item.title}</div>
+      ))}
     </>
   );
 }
