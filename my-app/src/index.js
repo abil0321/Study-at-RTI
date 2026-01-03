6429c5fc2b2fd2b1242b2a7cf9355b92cb24299f
Abil <salsabilmuhammad45@gmail.com>
2026-01-03 18:13:26 +0800

    feat: Add global styles and layout for the application
    - Introduced a new CSS file for global styles.
    - Set up a responsive layout with a flexbox container.
    - Defined styles for header, menu, food items, and footer.
    - Implemented a spacing and font size system for consistency.
    - Included Google Fonts for typography.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,113 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDom from "react-dom/client";
+import "./index.css";
+// import "./../public/data.js";
 
 function App() {
-  return <h1>Hello, React Bree</h1>;
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  const style = {
+    color: "red",
+    fontSize: "50px",
+    textTransform: "uppercase",
+  };
+  return <h1 style={style}>Warteg Jamaluddin M-Top</h1>;
+}
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Menu Kita</h2>
+      <div className="foods">
+        <Food
+          nama={"Soto Betawi"}
+          deskripsi={"soto betawi dari betawi lah"}
+          foto={"food/soto-betawi.jpg"}
+          harga={20000}
+          stok={Math.random() >= 0.5 ? true : false}
+        />
+        <Food
+          nama={"Soto Betawi"}
+          deskripsi={"soto betawi dari betawi lah"}
+          foto={"food/soto-betawi.jpg"}
+          harga={20000}
+          stok={Math.random() >= 0.5 ? true : false}
+        />
+        <Food
+          nama={"Soto Betawi"}
+          deskripsi={"soto betawi dari betawi lah"}
+          foto={"food/soto-betawi.jpg"}
+          harga={20000}
+          stok={Math.random() >= 0.5 ? true : false}
+        />
+        <Food
+          nama={"Soto Betawi"}
+          deskripsi={"soto betawi dari betawi lah"}
+          foto={"food/soto-betawi.jpg"}
+          harga={20000}
+          stok={Math.random() >= 0.5 ? true : false}
+        />
+      </div>
+    </main>
+  );
+}
+function Footer() {
+  const [Hour, setHour] = useState(new Date().getHours());
+  const jamBuka = 10;
+  const jamTutup = 22;
+
+  useEffect(() => {
+    setInterval(() => {
+      setHour(new Date().getHours());
+    }, 1000);
+  }, []);
+
+  return (
+    <footer className="footer" style={{ textAlign: "center" }}>
+      <small style={{ textAlign: "center" }}>
+        {Hour >= jamBuka && Hour <= jamTutup
+          ? "Warteg Jamaluddin M-Top udah buka guys 🥳"
+          : "Warteg Jamaluddin M-Top lagi tutup Guys 🥲"}
+      </small>
+      <br />
+      <small>
+        {new Date().getFullYear()} Warung Jamaluddin M-Top | Jam Buka {jamBuka}{" "}
+        - Jam Tutup {jamTutup}
+      </small>
+    </footer>
+  );
+}
+function Food(props) {
+  return (
+    <div className="food">
+      <img src={props.foto} width={100} height={70} alt={props.nama} />
+      <div>
+        <h2>
+          {props.nama} - {props.stok ? "For Sale" : "Sold Out!"}
+        </h2>
+        <p>{props.deskripsi}</p>
+        <Rupiah angka={props.harga} />
+      </div>
+    </div>
+  );
+}
+
+function Rupiah(props) {
+  const formatAngka = new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    maximumFractionDigits: 0,
+    minimumFractionDigits: 0,
+  }).format(props.angka);
+
+  return <span>{props.angka ? formatAngka : "not for sale"}</span>;
 }
 
 const root = ReactDom.createRoot(document.getElementById("root"));
