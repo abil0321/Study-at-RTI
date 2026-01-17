162e59a9904338dfb99c5e2a23b112ad2aed7b44
Abil <salsabilmuhammad45@gmail.com>
2026-01-17 21:34:05 +0700

    feat: Initialize React application with to-do list functionality and update README to use pnpm commands.

--- src/App.js
+++ src/App.js
@@ -1,90 +1,190 @@
 import { useState } from "react";
 
 function App() {
-  const [color, setColor] = useState("#000000");
-  const [opacity, setOpacity] = useState(1);
+  const [items, setItems] = useState([]);
+  function handleAddData(item) {
+    setItems((items) => [...items, item]);
+  }
+  /* NOTE: delete note, dengan filter berdasarkan 'id' yang tidak sama atau pernah di pilih/click di handleDeleteNote(id) */
+  function handleDeleteItem(id) {
+    const updateItems = items.filter((item) => item.id !== id);
+    setItems(updateItems);
+  }
 
-  const handleColorChange = (event) => {
-    setColor(event.target.value);
-  };
-  const handleOpacityChange = (event) => {
-    const opacityValue = Number(event.target.value);
+  // SECTION: Finish Item (checkbox: conditional ✅❌)
+  function handleFinishItem(id) {
+    // NOTE: Update data dengan spread operator
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, done: !item.done } : item,
+      ),
+    );
 
-    setOpacity(opacityValue);
-  };
-  const getOpacityPercentage = () => {
-    const opacityPercentage = Math.round(opacity * 100);
-    return `${opacityPercentage}%`;
-  };
-  const getRGB = () => {
-    const red = parseInt(color.slice(1, 3), 16);
-    const green = parseInt(color.slice(3, 5), 16);
-    const blue = parseInt(color.slice(5, 7), 16);
+    // NOTE: Cara lama
+    // setItems((items) => {
+    //   return items.map((item) => {
+    //     if (item.id === id) {
+    //       return {
+    //         ...item,
+    //         done: !item.done,
+    //       };
+    //     }
+    //     return item;
+    //   });
+    // });
+  }
 
-    return `rgb(${red}, ${green}, ${blue})`;
-  };
-
-  const getRGBA = () => {
-    const red = parseInt(color.slice(1, 3), 16);
-    const green = parseInt(color.slice(3, 5), 16);
-    const blue = parseInt(color.slice(5, 7), 16);
-
-    return `rgba(${red}, ${green}, ${blue}, ${opacity})`;
-  };
-
-  const getHEXA = () => {
-    // Mengubah opacity (0-1) menjadi angka 0-255, lalu ke Hex (00-FF)
-    const alpha = Math.round(opacity * 255)
-      // NOTE: // Kenapa dikali 255? Dalam dunia warna digital (8-bit), nilai maksimal adalah 255. Opacity cuma 0-1, jadi jika opacity 0.5 -> 0.5 * 255 = 127.5.
-      .toString(16) // NOTE: Mengubah angka desimal (basis 10) menjadi Hexadecimal (basis 16).
-      // NOTE: Contoh: Angka 128 kalau diubah ke Hex menjadi "80". (255 -> ff, 10 -> a)
-      .padStart(2, "0") // NOTE: Ini untuk menjaga format agar selalu 2 digit ("e" -> menjadi "0e").
-      .toUpperCase(); // NOTE: Mengubah huruf kecil menjadi huruf besar agar terlihat rapi dan standar (ff -> FF).
-    return `${color}${alpha}`;
-  };
-
-  const getCSSCode = () => {
-    const cssCode = `
-    background-color = ${color};
-    opacity = ${opacity};
-    /* Add another style ... */
-    `;
-
-    return cssCode.trim();
-  };
+  const count_items = items.length;
 
   return (
-    <div className="App">
-      <h1>Color Generator</h1>
-      <input type="color" value={color} onChange={handleColorChange} />
-      <br />
-      <input
-        type="range"
-        min="0"
-        max="1"
-        step="0.01"
-        value={opacity}
-        onChange={handleOpacityChange}
+    <div className="app">
+      <Logo />
+      <Form onAddItem={handleAddData} countItems={count_items} />
+      <CheckList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onFinishItem={handleFinishItem}
       />
-      {color && (
-        <div
-          className="color-box"
-          style={{ backgroundColor: color, opacity: opacity }}
-        ></div>
-      )}
-      {color && (
-        <div className="color-info">
-          <p>Hex: {color}</p>
-          <p>Hexa: {getHEXA()}</p>
-          <p>RGB: {getRGB()}</p>
-          <p>RGBA: {getRGBA()}</p>
-          <p>Opacity: {getOpacityPercentage()}</p>
-          <pre>
-            <code>{getCSSCode()}</code>
-          </pre>
-        </div>
-      )}
+      <Stats listItems={items} />
     </div>
+  );
+}
+
+function Logo() {
+  return <span className="logo">👨‍💻 GoCheck ✅</span>;
+}
+
+function Form({ onAddItem, countItems }) {
+  const [title, setTitle] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (title.trim() === "") {
+      return;
+    }
+
+    let countListItem = countItems + 1;
+    const newData = {
+      id: countListItem,
+      title: title,
+      done: false,
+    };
+    onAddItem(newData);
+    setTitle("");
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>Ada yang mau kamu catat ga? 😅</h3>
+      <input
+        type="text"
+        name="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        id=""
+      />
+      <button>ADD</button>
+    </form>
+  );
+}
+function CheckList({ items, onDeleteItem, onFinishItem }) {
+  //* ANCHOR: Sorting items
+  const [sortBy, setSortBy] = useState("input"); // NOTE: state, Default sort by input
+
+  // NOTE: Function untuk sorting items
+  function sortItems() {
+    switch (sortBy) {
+      case "title":
+        return items.slice().sort((a, b) => a.title.localeCompare(b.title));
+      case "status":
+        return items.slice().sort((a, b) => Number(a.done) - Number(b.done));
+      case "input":
+      default:
+        return items;
+    }
+  }
+
+  // NOTE: menampung hasil sorting items dan sekalian implementasi derived state👌
+  const sortedItems = sortItems();
+
+  return (
+    <div className="list">
+      <ul>
+        {/* // NOTE: mapping items */}
+        {sortedItems.map((item) => (
+          <Item
+            key={item.id}
+            data={item}
+            handleDelete={onDeleteItem} // NOTE: fungsi untuk menghapus data dijadikan props
+            handleFinish={onFinishItem}
+          />
+        ))}
+      </ul>
+
+      <div className="actions">
+        {/* // NOTE: Select and set sorting items */}
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Urutkan berdasarkan Input</option>
+          <option value="title">Urutkan berdasarkan Title</option>
+          <option value="status">Urutkan berdasarkan Status</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+function Item({ data, handleDelete, handleFinish }) {
+  const { id, title, done } = data;
+  return (
+    <>
+      <li>
+        <input
+          type="checkbox"
+          checked={done}
+          onChange={() => {
+            handleFinish(id);
+          }}
+        />
+        <span
+          onClick={() => handleFinish(id)}
+          className={`${done ? "lineChecked" : ""}`}
+        >
+          {title}
+        </span>
+        <span onClick={() => handleFinish(id)}>{done ? "✅" : "❌"}</span>
+        {/* <button onClick={() => setIsOpen(!isOpen)}>Remove</button> */}
+        <button onClick={() => handleDelete(id)}>Remove</button>{" "}
+        {/* NOTE: Melakukan lifting state biar dia diatas karena loopingnya ga di sini cuy */}
+      </li>
+    </>
+  );
+}
+
+function Stats({ listItems }) {
+  const count_items = listItems.length;
+  const count_done_item = listItems.filter((item) => item.done === true).length;
+  const percentage_done_item = Math.round(
+    (count_done_item / count_items) * 100,
+  );
+
+  if (count_items < 1) {
+    return (
+      <footer className="stats">
+        <span>
+          🗒️ Kamu belum memiliki plan hari ini, yuk mulai bikin catatan!
+        </span>
+      </footer>
+    );
+  }
+  return (
+    <footer className="stats">
+      <span>
+        {percentage_done_item === 100
+          ? `Sudah kelar semua dari ${count_items} task, Mantap 🫡👌`
+          : `🗒️ Kamu punya ${count_items} catatan dan baru ${count_done_item} yang dichecklist (${percentage_done_item}%) ✅`}
+      </span>
+    </footer>
   );
 }
 
