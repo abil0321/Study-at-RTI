f23c9ec73f2a64d352be5e7332d59ada9d8cc635
Abil <salsabilmuhammad45@gmail.com>
2026-01-18 10:24:16 +0700

    feat: Implement a basic checklist application with item creation, management, sorting, and statistics.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import Logo from "./components/Logo";
+import Form from "./components/Form";
+import CheckList from "./components/Checklist";
+import Stats from "./components/Stats";
 
 function App() {
   const [items, setItems] = useState([]);
@@ -34,6 +38,14 @@ function App() {
     // });
   }
 
+  function handleClearItems() {
+    const confirm = window.confirm("Apakah kamu mau menghapus semua list ini?");
+    if (confirm) {
+      setItems([]);
+    }
+    // choose_to_clear = window.confirm("Apakah kamu ingin membersihkan semua ");
+  }
+
   const count_items = items.length;
 
   return (
@@ -44,147 +56,10 @@ function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onFinishItem={handleFinishItem}
+        onClearItems={handleClearItems}
       />
       <Stats listItems={items} />
     </div>
-  );
-}
-
-function Logo() {
-  return <span className="logo">👨‍💻 GoCheck ✅</span>;
-}
-
-function Form({ onAddItem, countItems }) {
-  const [title, setTitle] = useState("");
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    if (title.trim() === "") {
-      return;
-    }
-
-    let countListItem = countItems + 1;
-    const newData = {
-      id: countListItem,
-      title: title,
-      done: false,
-    };
-    onAddItem(newData);
-    setTitle("");
-  }
-
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>Ada yang mau kamu catat ga? 😅</h3>
-      <input
-        type="text"
-        name="title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        id=""
-      />
-      <button>ADD</button>
-    </form>
-  );
-}
-function CheckList({ items, onDeleteItem, onFinishItem }) {
-  //* ANCHOR: Sorting items
-  const [sortBy, setSortBy] = useState("input"); // NOTE: state, Default sort by input
-
-  // NOTE: Function untuk sorting items
-  function sortItems() {
-    switch (sortBy) {
-      case "title":
-        return items.slice().sort((a, b) => a.title.localeCompare(b.title));
-      case "status":
-        return items.slice().sort((a, b) => Number(a.done) - Number(b.done));
-      case "input":
-      default:
-        return items;
-    }
-  }
-
-  // NOTE: menampung hasil sorting items dan sekalian implementasi derived state👌
-  const sortedItems = sortItems();
-
-  return (
-    <div className="list">
-      <ul>
-        {/* // NOTE: mapping items */}
-        {sortedItems.map((item) => (
-          <Item
-            key={item.id}
-            data={item}
-            handleDelete={onDeleteItem} // NOTE: fungsi untuk menghapus data dijadikan props
-            handleFinish={onFinishItem}
-          />
-        ))}
-      </ul>
-
-      <div className="actions">
-        {/* // NOTE: Select and set sorting items */}
-        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-          <option value="input">Urutkan berdasarkan Input</option>
-          <option value="title">Urutkan berdasarkan Title</option>
-          <option value="status">Urutkan berdasarkan Status</option>
-        </select>
-      </div>
-    </div>
-  );
-}
-
-function Item({ data, handleDelete, handleFinish }) {
-  const { id, title, done } = data;
-  return (
-    <>
-      <li>
-        <input
-          type="checkbox"
-          checked={done}
-          onChange={() => {
-            handleFinish(id);
-          }}
-        />
-        <span
-          onClick={() => handleFinish(id)}
-          className={`${done ? "lineChecked" : ""}`}
-        >
-          {title}
-        </span>
-        <span onClick={() => handleFinish(id)}>{done ? "✅" : "❌"}</span>
-        {/* <button onClick={() => setIsOpen(!isOpen)}>Remove</button> */}
-        <button onClick={() => handleDelete(id)}>Remove</button>{" "}
-        {/* NOTE: Melakukan lifting state biar dia diatas karena loopingnya ga di sini cuy */}
-      </li>
-    </>
-  );
-}
-
-function Stats({ listItems }) {
-  const count_items = listItems.length;
-  const count_done_item = listItems.filter((item) => item.done === true).length;
-  const percentage_done_item = Math.round(
-    (count_done_item / count_items) * 100,
-  );
-
-  if (count_items < 1) {
-    return (
-      <footer className="stats">
-        <span>
-          🗒️ Kamu belum memiliki plan hari ini, yuk mulai bikin catatan!
-        </span>
-      </footer>
-    );
-  }
-  return (
-    <footer className="stats">
-      <span>
-        {percentage_done_item === 100
-          ? `Sudah kelar semua dari ${count_items} task, Mantap 🫡👌`
-          : `🗒️ Kamu punya ${count_items} catatan dan baru ${count_done_item} yang dichecklist (${percentage_done_item}%) ✅`}
-      </span>
-    </footer>
   );
 }
 
